014804692345035998a2f634129e3bf641f47d1a
appulalps <shemil.raveendran@medgenome.com>
2019-06-01 00:05:08 +0530

    Show images in the comment

--- wp-content/plugins/comment_analyzer/admin.php
+++ wp-content/plugins/comment_analyzer/admin.php
@@ -32,8 +32,8 @@ if (!class_exists("CommentAnalyserAdmin")) {
 			if (isset($_POST['update_commentAnalyzerSettings'])) {
 				$commentAnalyzerOptions['admin'] 		= apply_filters('keyword_save_pre', $_POST['admin']);
 				$commentAnalyzerOptions['frontend'] 	= apply_filters('keyword_save_pre', $_POST['frontend']);
-				$commentAnalyzerOptions['apikey'] 	= apply_filters('keyword_save_pre', $_POST['apikey']);
-				$commentAnalyzerOptions['apiurl'] 	= apply_filters('keyword_save_pre', $_POST['apiurl']);
+				$commentAnalyzerOptions['apikey'] 		= apply_filters('keyword_save_pre', $_POST['apikey']);
+				$commentAnalyzerOptions['apiurl'] 		= apply_filters('keyword_save_pre', $_POST['apiurl']);
 				update_option($this->adminOptionsName, $commentAnalyzerOptions);
 				?>
 				<div id="message" class="updated settings-error notice is-dismissible">
@@ -50,37 +50,36 @@ if (!class_exists("CommentAnalyserAdmin")) {
 				<div class="wrap">
 				
 					<div style="float:left;width:100%">
-						<form method="post" action="<?php echo admin_url('admin.php?page=comment-analyzer')?>">
+					<form method="post" action="<?php echo admin_url('admin.php?page=comment-analyzer')?>">
 						<h2><img src="<?php echo plugins_url('images/settings.png', __FILE__ )?>" align="absbottom" />&nbsp;Comment Analyzer</h2>
 						<h3>Comment Analyzer Settings</h3>
-			<table class="form-table analyzer-settings">
-				<tbody>
-				 <tr class="form-field form-required">
-				  <th scope="row">Show analyzer icon on front end </th>
-				  <td><label for="send_password"><input type="checkbox" name="frontend" <?php checked('1', $commentAnalyzerOptions['frontend']); ?> value="1" ></label></td>
-				 </tr>
-				 <tr class="form-field form-required">
-				  <th scope="row">Show comment analyzer icon on admin side </th>
-				  <td><input type="checkbox" name="admin" <?php checked('1', $commentAnalyzerOptions['admin']); ?> value="1" ></td>
-				 </tr>
-				 <tr class="form-field form-required">
-				  <th scope="row">Meaningcloud.com API key <span class="description">(required)</span></th>
-				  <td><input type="text" name="apikey" class="input_kwd"  value="<?php echo esc_attr($commentAnalyzerOptions['apikey']); ?>" required />
-					<br>  <p class="description">Please request for api key <a href="https://www.meaningcloud.com/developer/login/" target="_blank">here</a></p></td>
-				 </tr>
-				 <tr class="form-field form-required">
-				  <th scope="row">Sentimental analysis API Url <span class="description">(required)</span></th>
-				  <td><input type="text" name="apiurl" class="input_kwd" value="<?php echo esc_attr($commentAnalyzerOptions['apiurl']); ?>" required />
-					  <br> <p class="description">Sample Url - https://api.meaningcloud.com/sentiment-2.1</p></td>
-				 </tr>
-				 
-				</tbody>
-			</table>
-			<p class="submit">
-				<input type="submit" name="update_commentAnalyzerSettings" value="Update Settings" class="button button-primary"></p>
-			</form>
-		  </div>
-	   </div>
+						<table class="form-table analyzer-settings">
+							<tbody>
+							 <tr class="form-field form-required">
+							  <th scope="row">Show analyzer icon on front end </th>
+							  <td><label for="send_password"><input type="checkbox" name="frontend" <?php checked('1', $commentAnalyzerOptions['frontend']); ?> value="1" ></label></td>
+							 </tr>
+							 <tr class="form-field form-required">
+							  <th scope="row">Show comment analyzer icon on admin side </th>
+							  <td><input type="checkbox" name="admin" <?php checked('1', $commentAnalyzerOptions['admin']); ?> value="1" ></td>
+							 </tr>
+							 <tr class="form-field form-required">
+							  <th scope="row">Meaningcloud.com API key <span class="description">(required)</span></th>
+							  <td><input type="text" name="apikey" class="input_kwd"  value="<?php echo esc_attr($commentAnalyzerOptions['apikey']); ?>" required />
+								<br>  <p class="description">Please request for api key <a href="https://www.meaningcloud.com/developer/login/" target="_blank">here</a></p></td>
+							 </tr>
+							 <tr class="form-field form-required">
+							  <th scope="row">Sentimental analysis API Url <span class="description">(required)</span></th>
+							  <td><input type="text" name="apiurl" class="input_kwd" value="<?php echo esc_attr($commentAnalyzerOptions['apiurl']); ?>" required />
+								  <br> <p class="description">Sample Url - https://api.meaningcloud.com/sentiment-2.1</p></td>
+							 </tr>
+							</tbody>
+						</table>
+						<p class="submit">
+						<input type="submit" name="update_commentAnalyzerSettings" value="Update Settings" class="button button-primary"></p>
+					</form>
+				</div>
+			</div>
 		<?php
 	  }
 
@@ -143,4 +142,39 @@ function display_comment_analyzer_options() {
 			return;
 	}
 	$ObjCommentAnalyzerPlugin->printAdminPage();
+}
+
+function add_comment_analyzer_images_with_comments($comment_text,$comment_ID) {
+	$commentAnalyzerOptions 	= get_option('comment_analyzer_options');
+	$comment			= get_comment($comment_ID);
+	if (is_admin()){
+		if ($commentAnalyzerOptions['admin']) {
+			if ($comment->comment_analyzer_value == 'neutral')
+				$commentAnalyzerImage = '<img src="'.plugins_url('images/neutral.png', __FILE__).'" title="Neutral">';
+			else if ($comment->comment_analyzer_value == 'good')
+				$commentAnalyzerImage = '<img src="'.plugins_url('images/good.png', __FILE__).'" title="Good">';
+			else if ($comment->comment_analyzer_value == 'bad')
+				$commentAnalyzerImage = '<img src="'.plugins_url('images/bad.png', __FILE__).'" title="Bad">';
+			else
+				$commentAnalyzerImage = '';
+			return $commentAnalyzerImage.' '.$comment_text;
+		}else{
+			return $comment_text;
+		}
+	}
+	else{
+		if ($commentAnalyzerOptions['frontend']) {
+			if ($comment->comment_analyzer_value == 'neutral')
+				$commentAnalyzerImage = '<img src="'.plugins_url('images/neutral.png', __FILE__).'" title="Neutral">';
+			else if ($comment->comment_analyzer_value == 'good')
+				$commentAnalyzerImage = '<img src="'.plugins_url('images/good.png', __FILE__).'" title="Good">';
+			else if ($comment->comment_analyzer_value == 'bad')
+				$commentAnalyzerImage = '<img src="'.plugins_url('images/bad.png', __FILE__).'" title="Bad">';
+			else
+				$commentAnalyzerImage = '';
+			return $commentAnalyzerImage.' '.$comment_text;
+		}else{
+			return $comment_text;
+		}
+	}
 }
